ee3e35e1ce84d25536cb2caf68f2d6cadb83cf53
rahul <rahulp5875@gmail.com>
2024-06-10 13:44:31 +0530

    Code clean up in main activity.

--- app/src/main/java/com/example/composelandmarkai/ui/MainActivity.kt
+++ app/src/main/java/com/example/composelandmarkai/ui/MainActivity.kt
@@ -14,12 +14,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -27,12 +24,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.composelandmarkai.ui.domain.Classification
-import com.example.composelandmarkai.ui.domain.LandmarkClassifier
 import com.example.composelandmarkai.ui.domain.TfLiteLandmarkClassifier
 import com.example.composelandmarkai.ui.presentation.CameraPreview
 import com.example.composelandmarkai.ui.presentation.LandmarkAnalyzer
